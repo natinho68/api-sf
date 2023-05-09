e206911794e7f186cf4f973f36cefa5984f7a8ef
7385810 <mandemons1@hotmail.com>
2023-05-09 18:14:35 +0200

    feat: add update/store for crud author & add location on store

--- src/Controller/AuthorController.php
+++ src/Controller/AuthorController.php
@@ -7,12 +7,32 @@ use App\Repository\AuthorRepository;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Routing\Generator\UrlGeneratorInterface;
+use Symfony\Component\Serializer\Normalizer\AbstractNormalizer;
 use Symfony\Component\Serializer\SerializerInterface;
 
 class AuthorController extends AbstractController
 {
+    #[Route('/api/authors', name: 'store_author', methods: ['POST'])]
+    public function store(Request $request, SerializerInterface $serializer, EntityManagerInterface $entityManager, UrlGeneratorInterface $urlGenerator): JsonResponse
+    {
+        $author = $serializer->deserialize($request->getContent(), Author::class, 'json');
+
+        $entityManager->persist($author);
+        $entityManager->flush();
+
+        $location = $urlGenerator->generate('show_author', ['author' => $author->getId()], UrlGeneratorInterface::ABSOLUTE_URL);
+
+        return new JsonResponse(
+            null,
+            Response::HTTP_NO_CONTENT,
+            ['location' => $location]
+        );
+    }
+
     #[Route('/api/authors', name: 'index_author', methods: ['GET'])]
     public function index(AuthorRepository $authorRepository, SerializerInterface $serializer): JsonResponse
     {
@@ -33,6 +53,17 @@ class AuthorController extends AbstractController
             [],
             true
         );
+    }
+
+    #[Route('/api/authors/{author}', name: 'update_author', methods: ['PUT'])]
+    public function update(Author $author, Request $request, SerializerInterface $serializer, EntityManagerInterface $entityManager)
+    {
+        $updatedAuthor = $serializer->deserialize($request->getContent(), Author::class, 'json', [AbstractNormalizer::OBJECT_TO_POPULATE => $author]);
+
+        $entityManager->persist($updatedAuthor);
+        $entityManager->flush();
+
+        return new JsonResponse(null, Response::HTTP_NO_CONTENT);
     }
 
     #[Route('/api/authors/{author}', name: 'destroy_author', methods: ['DELETE'])]
