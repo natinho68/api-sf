687a1390d7c02c3a273ccd808b54ff5f3019453f
7385810 <mandemons1@hotmail.com>
2023-05-10 11:24:04 +0200

    feat: add exception subscriber

---
+++ src/EventSubscriber/ExceptionSubscriber.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace App\EventSubscriber;
+
+use Symfony\Component\EventDispatcher\EventSubscriberInterface;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpKernel\Event\ExceptionEvent;
+use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
+use Symfony\Component\HttpKernel\KernelEvents;
+
+class ExceptionSubscriber implements EventSubscriberInterface
+{
+    public function onKernelException(ExceptionEvent $event): void
+    {
+        $exception = $event->getThrowable();
+
+        if ($exception instanceof NotFoundHttpException) {
+            $statusCode = $exception->getStatusCode();
+        } else {
+            $statusCode = Response::HTTP_INTERNAL_SERVER_ERROR;
+        }
+
+        $data = [
+            'status' => $statusCode,
+            'message' => $exception->getMessage(),
+        ];
+
+        $event->setResponse(new JsonResponse($data, $statusCode));
+    }
+
+    public static function getSubscribedEvents(): array
+    {
+        return [
+            KernelEvents::EXCEPTION => 'onKernelException',
+        ];
+    }
+}
